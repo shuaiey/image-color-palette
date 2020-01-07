dea2ad7ee08369e74e7ab64f26526ca9cc064f49
Echo Yang <echoyyyang@gamil.com>
2020-01-07 20:49:29 +0100

    Updates

---
+++ precache-manifest.e3e95119f0758089d305d35ddfcb9390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885cd8099120c9aa350501b40bec01aa",
+    "url": "/image-color-palette/index.html"
+  },
+  {
+    "revision": "8ace2f2bd806b6312db3",
+    "url": "/image-color-palette/static/css/main.c14bfedc.chunk.css"
+  },
+  {
+    "revision": "54a6910c3e62d1d6c9c3",
+    "url": "/image-color-palette/static/js/2.e5719e89.chunk.js"
+  },
+  {
+    "revision": "8ace2f2bd806b6312db3",
+    "url": "/image-color-palette/static/js/main.524f3bfd.chunk.js"
+  },
+  {
+    "revision": "9a18d25530a42b03fa9f",
+    "url": "/image-color-palette/static/js/runtime~main.231fe424.js"
+  }
+]);
